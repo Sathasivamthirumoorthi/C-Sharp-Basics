75afedb5a173df28a7ad7a72420e51c0e2d3b0b3
Sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2023-04-26 17:52:48 -0700

    abstraction

--- Abstraction/Marks.cs
+++ Abstraction/Marks.cs
@@ -6,42 +6,42 @@ namespace MyProblem{
         public abstract double getPercentage();
     }
 
-    class StudentA : Marks{
+    class CSE : Marks{
 
-        private int subject1;
-        private int subject2;
-        private int subject3;
+        private int maths;
+        private int compilerDesign;
+        private int evs;
 
-        public StudentA(int subject1, int subject2, int subject3){
-            this.subject1 = subject1;
-            this.subject2 = subject2;
-            this.subject3 = subject3;
+        public CSE(int maths, int compilerDesign, int evs){
+            this.maths = maths;
+            this.compilerDesign = compilerDesign;
+            this.evs = evs;
 
         }
         public override double getPercentage()
         {
-            double total = (subject1 + subject2 + subject3);
+            double total = (maths + compilerDesign + evs);
             double percentage = (total / 300.0) * 100;
             return Math.Round(percentage,2);;
         }
     }
 
-     class StudentB : Marks{
+     class IT : Marks{
 
-        private int subject1;
-        private int subject2;
-        private int subject3;
-        private int subject4;
+        private int maths;
+        private int compilerDesign;
+        private int evs;
+        private int python;
 
-        public StudentB(int subject1, int subject2, int subject3 , int subject4){
-            this.subject1 = subject1;
-            this.subject2 = subject2;
-            this.subject3 = subject3;
-            this.subject4 = subject4;
+        public IT(int maths, int compilerDesign, int evs , int python){
+            this.maths = maths;
+            this.compilerDesign = compilerDesign;
+            this.evs = evs;
+            this.python = python;
         }
         public override double getPercentage()
         {
-            double total = (subject1 + subject2 + subject3 + subject4);
+            double total = (maths + compilerDesign + evs + python);
             double percentage = (total / 400.0) * 100;
             return Math.Round(percentage,2);
         }
@@ -49,10 +49,10 @@ namespace MyProblem{
 
     // class TestMarks{
     //     public static void Main(string[] args){
-    //         StudentA shiva = new StudentA(70,90,90);
+    //         CSE shiva = new CSE(70,90,90);
     //         Console.WriteLine(" Percentage {0} ",shiva.getPercentage());
 
-    //         StudentB Ari = new StudentB(99,99,99,99);
+    //         IT Ari = new IT(99,99,99,99);
     //         Console.WriteLine(" Percentage {0} ",Ari.getPercentage());
     //     }
     // }
